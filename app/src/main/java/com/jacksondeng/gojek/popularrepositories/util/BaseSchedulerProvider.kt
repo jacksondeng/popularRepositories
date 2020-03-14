4b134c2175f837b24e85de34d48de8c75f2afa9a
jacksondeng <jacksondeng92@gmail.com>
2020-03-14 16:44:25 +0800

    added BaseSchedulerProvider for testing api calls

---
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/util/BaseSchedulerProvider.kt
@@ -0,0 +1,35 @@
+package com.jacksondeng.gojek.popularrepositories.util
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.TestScheduler
+import java.util.concurrent.Executors
+
+interface BaseSchedulerProvider {
+    fun io(): Scheduler
+    fun computation(): Scheduler
+    fun ui(): Scheduler
+    fun singleThread(): Scheduler
+}
+
+class SchedulerProvider : BaseSchedulerProvider {
+    override fun computation() = Schedulers.computation()
+    override fun ui() = AndroidSchedulers.mainThread()
+    override fun io() = Schedulers.io()
+    override fun singleThread() = Schedulers.from(Executors.newSingleThreadExecutor())
+}
+
+class TrampolineSchedulerProvider : BaseSchedulerProvider {
+    override fun computation() = Schedulers.trampoline()
+    override fun ui() = Schedulers.trampoline()
+    override fun io() = Schedulers.trampoline()
+    override fun singleThread() = Schedulers.trampoline()
+}
+
+class TestSchedulerProvider(private val scheduler: TestScheduler) : BaseSchedulerProvider {
+    override fun computation() = scheduler
+    override fun ui() = scheduler
+    override fun io() = scheduler
+    override fun singleThread() = scheduler
+}
