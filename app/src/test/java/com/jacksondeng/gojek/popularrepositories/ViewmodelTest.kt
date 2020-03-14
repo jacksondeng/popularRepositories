f149a60db12c7221fc3a27689d29b9be46560f96
jacksondeng <jacksondeng92@gmail.com>
2020-03-14 17:21:43 +0800

    added some tests for viewmodel's state

---
+++ app/src/test/java/com/jacksondeng/gojek/popularrepositories/ViewmodelTest.kt
@@ -0,0 +1,71 @@
+package com.jacksondeng.gojek.popularrepositories
+
+import android.accounts.NetworkErrorException
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.jacksondeng.gojek.popularrepositories.data.repo.FetchRepositoriesRepo
+import com.jacksondeng.gojek.popularrepositories.model.entity.Repo
+import com.jacksondeng.gojek.popularrepositories.ui.main.FetchRepositoriesViewModel
+import com.jacksondeng.gojek.popularrepositories.util.State
+import com.jacksondeng.gojek.popularrepositories.util.TrampolineSchedulerProvider
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Flowable
+import org.junit.*
+
+class ViewmodelTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    private val repo = mockk<FetchRepositoriesRepo>(relaxed = true)
+
+    lateinit var viewModel: FetchRepositoriesViewModel
+
+    private val dummyResponse = listOf(
+        Repo(
+            author = "google",
+            name = "mediapipe",
+            avatarUrl = "https://github.com/google.png",
+            repoUrl = "https://github.com/google/mediapipe",
+            description = "MediaPipe is a cross-platform framework for building multimodal applied machine learning pipelines",
+            language = "C++",
+            languageColor = "#f34b7d",
+            starts = 5641,
+            forks = 977
+        )
+    )
+
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(FetchRepositoriesViewModel::class)
+        viewModel = FetchRepositoriesViewModel(repo)
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    internal fun `fetch repos success test`() {
+        val schedulerProvider = TrampolineSchedulerProvider()
+        every { repo.fetchRepositories(schedulerProvider) } returns Flowable.just(dummyResponse)
+        viewModel.fetchRepositories(schedulerProvider)
+        Assert.assertTrue(viewModel.state.getOrAwaitValue() == State.Loaded(dummyResponse))
+    }
+
+    @Test
+    internal fun `fetch repos failed test`() {
+        val schedulerProvider = TrampolineSchedulerProvider()
+        every { repo.fetchRepositories(schedulerProvider) } returns Flowable.error(
+            NetworkErrorException()
+        )
+        viewModel.fetchRepositories(schedulerProvider)
+        Assert.assertTrue(viewModel.state.getOrAwaitValue() == State.Error())
+    }
+}
