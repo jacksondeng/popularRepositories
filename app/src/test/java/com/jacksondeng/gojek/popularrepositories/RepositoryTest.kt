ccecd0a4c458fd3da9e8c8ef3407b5b2946239ee
jacksondeng <jacksondeng92@gmail.com>
2020-03-14 17:01:22 +0800

    added some tests for repository

--- app/src/test/java/com/jacksondeng/gojek/popularrepositories/RepositoryTest.kt
+++ app/src/test/java/com/jacksondeng/gojek/popularrepositories/RepositoryTest.kt
@@ -1,9 +1,12 @@
 package com.jacksondeng.gojek.popularrepositories
 
+import android.accounts.NetworkErrorException
 import com.jacksondeng.gojek.popularrepositories.data.api.FetchRepositoriesApi
 import com.jacksondeng.gojek.popularrepositories.data.repo.FetchRepositoriesRepoImpl
 import com.jacksondeng.gojek.popularrepositories.model.dto.BuilderDTO
 import com.jacksondeng.gojek.popularrepositories.model.dto.RepoDTO
+import com.jacksondeng.gojek.popularrepositories.model.entity.Repo
+import com.jacksondeng.gojek.popularrepositories.util.TrampolineSchedulerProvider
 import io.mockk.every
 import io.mockk.mockk
 import io.reactivex.Flowable
@@ -13,7 +16,6 @@ import io.reactivex.schedulers.Schedulers
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import java.util.*
 
 class RepositoryTest {
 
@@ -64,12 +66,40 @@ class RepositoryTest {
             dummyResponse
         )
 
-        /*val subscriber = repo.fetchRepositories()
+        val subscriber = repo.fetchRepositories(TrampolineSchedulerProvider())
             .test()
             .assertValue(
-
+                listOf(
+                    Repo(
+                        author = "google",
+                        name = "mediapipe",
+                        avatarUrl = "https://github.com/google.png",
+                        repoUrl = "https://github.com/google/mediapipe",
+                        description = "MediaPipe is a cross-platform framework for building multimodal applied machine learning pipelines",
+                        language = "C++",
+                        languageColor = "#f34b7d",
+                        starts = 5641,
+                        forks = 977
+                    )
+                )
             )
+
         subscriber.dispose()
-         */
+    }
+
+    @Test
+    internal fun `fetch repo failed no internet test`() {
+        every { api.fetchRepositories() } returns Flowable.error(NetworkErrorException())
+
+        val subscriber = repo.fetchRepositories( TrampolineSchedulerProvider())
+            .test()
+            .assertError {
+                println(it)
+                it is NetworkErrorException
+            }
+            .assertNoValues()
+            .assertTerminated()
+
+        subscriber.dispose()
     }
 }
