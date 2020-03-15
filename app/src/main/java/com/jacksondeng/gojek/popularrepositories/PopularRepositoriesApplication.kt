757860b108f166c212ba77ee4a1ae340061c02b6
jacksondeng <jacksondeng92@gmail.com>
2020-03-15 23:01:26 +0800

    fix: Fix crash related to multidex on kitkat

--- app/src/main/java/com/jacksondeng/gojek/popularrepositories/PopularRepositoriesApplication.kt
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/PopularRepositoriesApplication.kt
@@ -1,5 +1,7 @@
 package com.jacksondeng.gojek.popularrepositories
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.jacksondeng.gojek.popularrepositories.di.component.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -7,5 +9,10 @@ import dagger.android.DaggerApplication
 class PopularRepositoriesApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
