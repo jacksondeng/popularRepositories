b0e0d63ff5481e7e96e14d75be553b728591aa70
jacksondeng <jacksondeng92@gmail.com>
2020-03-15 16:28:32 +0800

    feat: added method in Dao to clear cache

--- app/src/main/java/com/jacksondeng/gojek/popularrepositories/data/cache/dao/RepoDao.kt
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/data/cache/dao/RepoDao.kt
@@ -1,9 +1,6 @@
 package com.jacksondeng.gojek.popularrepositories.data.cache.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.jacksondeng.gojek.popularrepositories.data.cache.db.TABLE_NAME_REPOS
 import com.jacksondeng.gojek.popularrepositories.model.entity.Repo
 import io.reactivex.Single
@@ -15,4 +12,7 @@ interface RepoDao {
 
     @Query("SELECT * FROM $TABLE_NAME_REPOS")
     fun getCachedRepos(): Single<List<Repo>>
+
+    @Query("DELETE FROM $TABLE_NAME_REPOS")
+    fun clearCache()
 }
