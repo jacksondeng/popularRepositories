4229d2d20738a228d9839b3200418343faf8a8d8
jacksondeng <jacksondeng92@gmail.com>
2020-03-15 20:14:07 +0800

    feat: Added check for network connection when clicking try again btn

--- app/src/main/java/com/jacksondeng/gojek/popularrepositories/util/Extensions.kt
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/util/Extensions.kt
@@ -1,13 +1,52 @@
 package com.jacksondeng.gojek.popularrepositories.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import android.os.Build
 import android.widget.TextView
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+
 
 fun TextView.leftDrawable(@DrawableRes id: Int = 0, @DimenRes sizeRes: Int) {
     val drawable = ContextCompat.getDrawable(context, id)
     val size = resources.getDimensionPixelSize(sizeRes)
     drawable?.setBounds(0, 0, size, size)
     this.setCompoundDrawables(drawable, null, null, null)
+}
+
+fun preLollipop(block: () -> Unit) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        block.invoke()
+    }
+}
+
+fun postLollipop(block: () -> Unit) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        block.invoke()
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun Context.registerNetworkCallback(onAvailableAction: () -> Unit, onLostAction: () -> Unit) {
+    val connectivityManger =
+        this.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    connectivityManger.registerNetworkCallback(
+        NetworkRequest.Builder().build(),
+        object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                onAvailableAction.invoke()
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                onLostAction.invoke()
+            }
+        }
+    )
 }
--- app/src/main/java/com/jacksondeng/gojek/popularrepositories/views/MainFragment.kt
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/views/MainFragment.kt
@@ -1,9 +1,12 @@
 package com.jacksondeng.gojek.popularrepositories.views
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,9 +16,7 @@ import androidx.recyclerview.widget.SimpleItemAnimator
 import com.jacksondeng.gojek.popularrepositories.R
 import com.jacksondeng.gojek.popularrepositories.databinding.MainFragmentBinding
 import com.jacksondeng.gojek.popularrepositories.model.entity.RepoItem
-import com.jacksondeng.gojek.popularrepositories.util.EventType
-import com.jacksondeng.gojek.popularrepositories.util.State
-import com.jacksondeng.gojek.popularrepositories.util.ViewModelFactory
+import com.jacksondeng.gojek.popularrepositories.util.*
 import com.jacksondeng.gojek.popularrepositories.viewmodel.FetchRepositoriesViewModel
 import com.jacksondeng.gojek.popularrepositories.views.adapter.RepositoriesAdapter
 import dagger.android.support.DaggerFragment
@@ -32,6 +33,7 @@ class MainFragment : DaggerFragment() {
     lateinit var viewModel: FetchRepositoriesViewModel
     private lateinit var binding: MainFragmentBinding
     private lateinit var repoAdapter: RepositoriesAdapter
+    private var isConnectedToNetwork = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,8 +43,16 @@ class MainFragment : DaggerFragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        postLollipop {
+            context?.registerNetworkCallback(onAvailableAction = {
+                isConnectedToNetwork = true
+            }, onLostAction = {
+                isConnectedToNetwork = false
+            })
+        }
         initVm()
         initViews()
     }
@@ -89,6 +99,14 @@ class MainFragment : DaggerFragment() {
             addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
             // Prevent imageview flickering when submitList is called
             (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+        }
+
+        binding.emptyLayout.btnTryAgain.setOnClickListener {
+            if (isConnectedToNetwork) {
+                viewModel.onRefresh()
+            } else {
+                Toast.makeText(context, context?.getString(R.string.no_internet_connection_msg), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
