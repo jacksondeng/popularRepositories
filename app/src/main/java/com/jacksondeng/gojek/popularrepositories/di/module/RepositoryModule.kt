f9b194e7cef8efb1a2600f8f721009d9950efe27
jacksondeng <jacksondeng92@gmail.com>
2020-03-15 15:48:49 +0800

    feat: Inject Dao to Repo

--- app/src/main/java/com/jacksondeng/gojek/popularrepositories/di/module/RepositoryModule.kt
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/di/module/RepositoryModule.kt
@@ -1,6 +1,11 @@
 package com.jacksondeng.gojek.popularrepositories.di.module
 
+import androidx.room.Room
+import com.jacksondeng.gojek.popularrepositories.PopularRepositoriesApplication
 import com.jacksondeng.gojek.popularrepositories.data.api.FetchRepositoriesApi
+import com.jacksondeng.gojek.popularrepositories.data.cache.dao.RepoDao
+import com.jacksondeng.gojek.popularrepositories.data.cache.db.DB_NAME
+import com.jacksondeng.gojek.popularrepositories.data.cache.db.RepoDb
 import com.jacksondeng.gojek.popularrepositories.data.repo.FetchRepositoriesRepo
 import com.jacksondeng.gojek.popularrepositories.data.repo.FetchRepositoriesRepoImpl
 import com.jacksondeng.gojek.popularrepositories.di.scope.FragmentScope
@@ -9,11 +14,25 @@ import dagger.Provides
 
 @Module
 class RepositoryModule {
+
+    @Provides
+    @FragmentScope
+    fun providesRoomDatabase(application: PopularRepositoriesApplication): RepoDb {
+        return Room.databaseBuilder(application, RepoDb::class.java, DB_NAME).build()
+    }
+
+    @Provides
+    @FragmentScope
+    fun providesProductDao(db: RepoDb): RepoDao {
+        return db.repoDao()
+    }
+
     @Provides
     @FragmentScope
     fun providesRepositoriesRepo(
-        api: FetchRepositoriesApi
+        api: FetchRepositoriesApi,
+        dao: RepoDao
     ): FetchRepositoriesRepo {
-        return FetchRepositoriesRepoImpl(api)
+        return FetchRepositoriesRepoImpl(api, dao)
     }
 }
