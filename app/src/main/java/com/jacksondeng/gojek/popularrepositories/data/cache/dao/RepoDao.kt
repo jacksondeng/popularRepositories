64354d59609cac51da6200526ab11e46151021e4
jacksondeng <jacksondeng92@gmail.com>
2020-03-15 15:14:26 +0800

    feat: Added Dao class and Db instance

---
+++ app/src/main/java/com/jacksondeng/gojek/popularrepositories/data/cache/dao/RepoDao.kt
@@ -0,0 +1,18 @@
+package com.jacksondeng.gojek.popularrepositories.data.cache.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.jacksondeng.gojek.popularrepositories.data.cache.db.TABLE_NAME_REPOS
+import com.jacksondeng.gojek.popularrepositories.model.entity.Repo
+import io.reactivex.Single
+
+@Dao
+interface RepoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun updateCache(repos: List<Repo>): Long
+
+    @Query("SELECT * FROM $TABLE_NAME_REPOS")
+    fun getCachedRepos(): Single<List<Repo>>
+}
